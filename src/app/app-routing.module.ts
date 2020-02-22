6469731ef4939fcdafe16e19a6efed9594455346
Alisher <ali.xay@mail.ru>
2020-02-23 01:33:59 +0500

    add student page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,10 +12,6 @@ const routes: Routes = [
     loadChildren: () => import('./pages/account/account.module').then(m => m.AccountModule)
   },
   {
-    path: 'signup',
-    loadChildren: () => import('./pages/signup/signup.module').then(m => m.SignUpModule)
-  },
-  {
     path: 'schedule',
     loadChildren: () => import('./pages/schedule/schedule.module').then(m => m.ScheduleModule)
   },
@@ -38,13 +34,18 @@ const routes: Routes = [
   {
     path: 'groups',
     loadChildren: () => import('./pages/groups/groups.module').then( m => m.GroupsPageModule)
-  },  {
+  },
+  {
     path: 'main',
     loadChildren: () => import('./pages/main/main.module').then( m => m.MainPageModule)
   },
   {
     path: 'main-sign-in',
     loadChildren: () => import('./pages/main-sign-in/main-sign-in.module').then( m => m.MainSignInPageModule)
+  },
+  {
+    path: 'add-teacher',
+    loadChildren: () => import('./pages/add-teacher/add-teacher.module').then( m => m.AddTeacherPageModule)
   }
 
 ];
